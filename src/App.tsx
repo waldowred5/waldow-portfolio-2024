12df9574c53767df6d241fb42ddd41a78a4e2d02
Waldow <dwaldow@me.com>
2024-05-13 15:09:47 +0100

    add todos and three

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// FEATURES TODO:
+// - Add & configure three.js
+// - Add three.js hero scene
+// - Add & configure Tailwind
+// - Add & configure PrimeReact
+// - Add & style navbar
+// - Add & style links to navbar
+// - Add three.js skills scene (HackerHero Orb)
+
+// CHORES TODO:
+// - Update favicon
+// - Update README
+
 function App() {
   const [count, setCount] = useState(0)
 
